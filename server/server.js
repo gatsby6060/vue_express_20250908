580b0b1816c14d68f828edc2b1ff4301e30d98f2
gatsby6060 <uniline123@naver.com>
2025-09-08 16:27:27 +0900

    커밋

--- server/server.js
+++ server/server.js
@@ -65,14 +65,14 @@ app.get('/emp/list', async (req, res) => {
   }
 });
 
-app.get('/stu/insert', async (req, res) => {
-  const { stuNo, name, dept } = req.query;
+app.get('/emp/delete', async (req, res) => {
+  const { empNo } = req.query;
 
   try {
     await connection.execute(
       // `INSERT INTO STUDENT (STU_NO, STU_NAME, STU_DEPT) VALUES (${stuNo}, '${name}', '${dept}')`,
-      `INSERT INTO STUDENT (STU_NO, STU_NAME, STU_DEPT) VALUES (:stuNo, :name, :dept)`,
-      [stuNo, name, dept],
+      `DELETE FROM EMP WHERE EMPNO = :empNo`,
+      [empNo],
       { autoCommit: true }
     );
     res.json({
@@ -83,6 +83,62 @@ app.get('/stu/insert', async (req, res) => {
     res.status(500).send('Error executing insert');
   }
 });
+
+
+
+
+app.get('/prof/list', async (req, res) => {
+  const { } = req.query;
+  try {
+    const result = await connection.execute(
+        `SELECT * FROM PROFESSOR`
+    );
+    const columnNames = result.metaData.map(column => column.name);
+    // 쿼리 결과를 JSON 형태로 변환
+    const rows = result.rows.map(row => {
+      // 각 행의 데이터를 컬럼명에 맞게 매핑하여 JSON 객체로 변환
+      const obj = {};
+      columnNames.forEach((columnName, index) => {
+        obj[columnName] = row[index];
+      });
+      return obj;
+    });
+    //리턴
+    res.json({
+        result : "success",
+        profList : rows
+    });
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).send('Error executing query');
+  }
+});
+
+
+
+
+app.get('/prof/delete', async (req, res) => {
+  const { profNo } = req.query;
+  
+  try {
+    await connection.execute(
+      // `INSERT INTO STUDENT (STU_NO, STU_NAME, STU_DEPT) VALUES (${stuNo}, '${name}', '${dept}')`,
+      // `DELETE FROM PROFESSOR WHERE PROFNO = :profNo`,
+      `DELETE FROM PROFESSOR WHERE PROFNO = '${profNo}'`,
+      [],// ${} 이방식 쓰려면 왼쪽 [] 를 이렇게 []로 해야함
+      { autoCommit: true }
+    );
+    res.json({
+        result : "success"
+    });
+  } catch (error) {
+    console.error('Error executing delete', error);
+    res.status(500).send('Error executing delete');
+  }
+});
+
+
+
 
 
 
